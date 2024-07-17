9c41f91f2c2cfa4dbac0dd51feb510666f6af5cf
TatianaShuvaeva <talisman1982@gmail.com>
2024-07-17 20:36:27 +0200

    new tests

--- cypress/e2e/hello_cypress.spec.js
+++ cypress/e2e/hello_cypress.spec.js
@@ -3,10 +3,20 @@
 
 describe('test', () => {
     it('should do subscribe to news letter', () => {
-        cy.visit('https://www.cypress.io');
-        cy.get('[data-cy="footer-newsletter"]').click();
-        cy.wait(1000);
-        cy.get('#subscribe_email').type('t_tali@gmail.com');
+        // cy.visit('https://de.wikipedia.org/');
+        // cy.get('#pt-login > a > span').click();
+        // cy.get('#wpName1').should('be.visible');
+        // cy.get('#wpName1').type('talismancccc');
+
+        // cy.get('#wpLoginAttempt').should('be.enabled')
+        cy.visit('https://www.automationexercise.com/');
+        cy.get('#susbscribe_email').should('be.visible');
+        cy.get('#susbscribe_email').type('test@test.test');
+        cy.get('#subscribe').click();
+        // [data-cy="footer-newsletter"]
+        // cy.wait(1000);
+        // cy.get('#subscribe_email').type('t_tali@gmail.com');#
+
         
     });
 })
